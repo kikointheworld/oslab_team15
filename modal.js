6d5bd5b2190f4304637f1850b33391514e03a497
kkkiko <popo9855@g.skku.edu>
2022-05-16 16:39:22 +0900

    makeCoordinateCSS
    make Coordinate CSS & integrate modal page

---
+++ modal.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  function onClick() {
+    document.querySelector(".modalWrap").style.display = "block";
+    document.querySelector(".blackBg").style.display = "block";
+  }
+  function offClick() {
+    document.querySelector(".modalWrap").style.display = "none";
+    document.querySelector(".blackBg").style.display = "none";
+  }
+
+  document.getElementById("modalBtn1").addEventListener("click", onClick);
+  document.getElementById("modalBtn2").addEventListener("click", onClick);
+  document.getElementById("modalBtn3").addEventListener("click", onClick);
+  document.getElementById("modalBtn4").addEventListener("click", onClick);
+  document.querySelector(".modalClose").addEventListener("click", offClick);
+};
