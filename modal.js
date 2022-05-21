e16089763c3d7eb464d696b6e85a21812f9bbf63
kkkiko <popo9855@g.skku.edu>
2022-05-22 02:48:03 +0900

    change CSS
    1. change overall font
    2. add shadow effect
    3. change header sytle

--- modal.js
+++ modal.js
@@ -1,28 +1,15 @@
 window.onload = function () {
-  /*function onClick() {
-    document.querySelector(".modalWrap").style.display = "block";
-    document.querySelector(".blackBg").style.display = "block";
-  }
-  function offClick() {
-    document.querySelector(".modalWrap").style.display = "none";
-    document.querySelector(".blackBg").style.display = "none";
-  }
-  */
-  for(let i=1; i < 5; i++) {
-    document.getElementById("modalBtn"+i).addEventListener("click", function() {
-      document.getElementById("modal"+i).style.display = "block";
-      document.querySelector(".blackBg").style.display = "block";
-    })
+  for (let i = 1; i < 5; i++) {
+    document
+      .getElementById("modalBtn" + i)
+      .addEventListener("click", function () {
+        document.getElementById("modal" + i).style.display = "block";
+        document.querySelector(".blackBg").style.display = "block";
+      });
 
-    document.getElementById("close"+i).addEventListener("click", function() {
-      document.getElementById("modal"+i).style.display = "none";
+    document.getElementById("close" + i).addEventListener("click", function () {
+      document.getElementById("modal" + i).style.display = "none";
       document.querySelector(".blackBg").style.display = "none";
-    })
+    });
   }
-  /*
-  document.getElementById("modalBtn1").addEventListener("click", onClick);
-  document.getElementById("modalBtn2").addEventListener("click", onClick);
-  document.getElementById("modalBtn3").addEventListener("click", onClick);
-  document.getElementById("modalBtn4").addEventListener("click", onClick); 
-  document.querySelector(".modalClose").addEventListener("click", offClick);*/
 };
